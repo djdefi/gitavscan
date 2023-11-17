30d7bcd18dfb4b4f56a039ecf28b3a17f36986b9
djdefi <djdefi@gmail.com>
2023-11-17 12:49:19 -0800

    Revert log change

--- gitscan.sh
+++ gitscan.sh
@@ -18,12 +18,9 @@
 #/    
 #/    Scan with additional clamscan options.
 #/      $ gitscan.sh --options "--max-filesize=1M"
-#/
+#/        
 
 set -o nounset -o pipefail
-
-LOG_FILE="gitscan.log"
-echo "Starting Git Repository Scan" > "$LOG_FILE"
 
 usage() {
   grep '^#/' < "$0" | cut -c 4-
@@ -60,7 +57,7 @@ done
 echo "Beginning scan..."
 
 if ! [ -d ".git" ]; then
-  echo "ERROR: Not a git repository, skipping history scan." | tee -a "$LOG_FILE"
+  echo "ERROR: Not a git repository, skipping history scan."
   exit 1
 fi
 
@@ -69,42 +66,42 @@ SCRIPT="/usr/bin/clamscan -ri --no-summary $ADDITIONAL_OPTIONS"
 TMP=$(mktemp -d -q)
 REPO=$(pwd)
 
-echo "Scanning working and .git directories..." | tee -a "$LOG_FILE"
+echo "Scanning working and .git directories..."
 output=$($SCRIPT)
 if [ $? -ne 0 ]; then
-  echo "Error during scanning working and .git directories" | tee -a "$LOG_FILE"
+  echo "Error during scanning working and .git directories"
 else
   if echo "$output" | grep -q "FOUND"; then
-    echo "Found malicious file in ref $(git rev-parse HEAD)" | tee -a /output.txt
-    echo "$output" | tee -a /output.txt
+    echo "Found malicious file in ref $(git rev-parse HEAD)"
+    echo "$output"
   fi
 fi
 
 if [[ "${FULL_SCAN:-}" = "true" ]]; then
   # clone the git repository
   pushd $TMP > /dev/null 2>&1
-  if ! git clone $REPO 2>> "$LOG_FILE" 1>&2; then
-    echo "Failed to clone repository: $REPO" | tee -a "$LOG_FILE"
+  if ! git clone $REPO 2>&1; then
+    echo "Failed to clone repository: $REPO"
     exit 1
   fi
   cd $(basename $REPO)
 
   # Process all blobs
   blobs=$(git rev-list --objects --all)
-  echo "Inspecting $blobs blobs..." | tee -a "$LOG_FILE"
+  echo "Inspecting $blobs blobs..."
   
   for blob in ${blobs}; do
     objtype=$(git cat-file -t ${blob})
     if [[ ${objtype} == "blob" ]]; then
       output=$(${SCRIPT} <(git cat-file blob ${blob}))
       if [ $? -ne 0 ]; then
-        echo "Error scanning blob: ${blob}" | tee -a "$LOG_FILE"
+        echo "Error scanning blob: ${blob}"
       elif echo "${output}" | grep -q "FOUND"; then
-        echo "Found malicious file in blob ${blob}" | tee -a /output.txt
-        echo "${output}" | tee -a /output.txt
+        echo "Found malicious file in blob ${blob}"
+        echo "${output}"
       fi
     else
-      echo "Skipping non-blob object: ${blob}" | tee -a "$LOG_FILE"
+      echo "Skipping non-blob object: ${blob}"
     fi
   done
 
@@ -114,9 +111,9 @@ if [[ "${FULL_SCAN:-}" = "true" ]]; then
 fi
 
 if [ -s "/output.txt" ]; then
-  echo "Scan finished with detections $(date)" | tee -a "$LOG_FILE"
-  cat /output.txt | tee -a "$LOG_FILE"
+  echo "Scan finished with detections $(date)"
+  cat /output.txt
   exit 1
 else
-  echo "Scan finished without detections $(date)" | tee -a "$LOG_FILE"
+  echo "Scan finished without detections $(date)"
 fi
