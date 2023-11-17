99d2787134b7c534c924c40080872a0d48997ee0
djdefi <djdefi@gmail.com>
2023-11-17 12:41:21 -0800

    Better error handle

--- gitscan.sh
+++ gitscan.sh
@@ -18,8 +18,12 @@
 #/    
 #/    Scan with additional clamscan options.
 #/      $ gitscan.sh --options "--max-filesize=1M"
-#/        
+#/
+
 set -o nounset -o pipefail
+
+LOG_FILE="gitscan.log"
+echo "Starting Git Repository Scan" > "$LOG_FILE"
 
 usage() {
   grep '^#/' < "$0" | cut -c 4-
@@ -30,7 +34,6 @@ FULL_SCAN="false"
 ADDITIONAL_OPTIONS=""
 VERBOSE_MODE="false"
 
-# read the options
 # read the options
 TEMP=$(getopt -o vf:o: --long verbose,full,options: -n "$0" -- "$@") || { usage; exit 1; }
 eval set -- "$TEMP"
@@ -57,7 +60,7 @@ done
 echo "Beginning scan..."
 
 if ! [ -d ".git" ]; then
-  echo "ERROR: Not a git repository, skipping history scan."
+  echo "ERROR: Not a git repository, skipping history scan." | tee -a "$LOG_FILE"
   exit 1
 fi
 
@@ -66,31 +69,42 @@ SCRIPT="/usr/bin/clamscan -ri --no-summary $ADDITIONAL_OPTIONS"
 TMP=$(mktemp -d -q)
 REPO=$(pwd)
 
-echo "Scanning working and .git directories..."
+echo "Scanning working and .git directories..." | tee -a "$LOG_FILE"
 output=$($SCRIPT)
+if [ $? -ne 0 ]; then
+  echo "Error during scanning working and .git directories" | tee -a "$LOG_FILE"
+else
   if echo "$output" | grep -q "FOUND"; then
     echo "Found malicious file in ref $(git rev-parse HEAD)" | tee -a /output.txt
     echo "$output" | tee -a /output.txt
   fi
+fi
 
 if [[ "${FULL_SCAN:-}" = "true" ]]; then
   # clone the git repository
   pushd $TMP > /dev/null 2>&1
-  git clone $REPO 2> /dev/null 1>&2
+  if ! git clone $REPO 2>> "$LOG_FILE" 1>&2; then
+    echo "Failed to clone repository: $REPO" | tee -a "$LOG_FILE"
+    exit 1
+  fi
   cd $(basename $REPO)
 
-  # Process blobs
-  blobs=$(git rev-list --objects --all --filter=blob:limit=100k)
-  echo "Inspecting $blobs blobs..."
+  # Process all blobs
+  blobs=$(git rev-list --objects --all)
+  echo "Inspecting $blobs blobs..." | tee -a "$LOG_FILE"
   
   for blob in ${blobs}; do
     objtype=$(git cat-file -t ${blob})
     if [[ ${objtype} == "blob" ]]; then
       output=$(${SCRIPT} <(git cat-file blob ${blob}))
-      if echo "${output}" | grep -q "FOUND"; then
+      if [ $? -ne 0 ]; then
+        echo "Error scanning blob: ${blob}" | tee -a "$LOG_FILE"
+      elif echo "${output}" | grep -q "FOUND"; then
         echo "Found malicious file in blob ${blob}" | tee -a /output.txt
         echo "${output}" | tee -a /output.txt
       fi
+    else
+      echo "Skipping non-blob object: ${blob}" | tee -a "$LOG_FILE"
     fi
   done
 
@@ -100,9 +114,9 @@ if [[ "${FULL_SCAN:-}" = "true" ]]; then
 fi
 
 if [ -s "/output.txt" ]; then
-  echo "Scan finished with detections $(date)"
-  cat /output.txt
+  echo "Scan finished with detections $(date)" | tee -a "$LOG_FILE"
+  cat /output.txt | tee -a "$LOG_FILE"
   exit 1
+else
+  echo "Scan finished without detections $(date)" | tee -a "$LOG_FILE"
 fi
-
-echo "Scan finished $(date)"
